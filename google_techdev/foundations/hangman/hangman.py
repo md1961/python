f68d32ca77452350010196c9543b9030cb5fd9db
Naoyuki Kumagai <kumagai@mimas>
2018-01-15 14:22:44 +0900

    [hangman] Display when correct.  Use String.format()

--- google_techdev/foundations/hangman/hangman.py
+++ google_techdev/foundations/hangman/hangman.py
@@ -10,17 +10,18 @@ n_tries = MAX_TRIES
 guesses = []
 while n_tries > 0:
     while True:
-        print('Your guess(' + str(n_tries) + ' left): ', end='')
+        print('Your guess({} left): '.format(n_tries), end='')
         ch = input().upper()
         if len(ch) == 1 and ch.isalpha() and ch not in guesses:
             break
     guesses.append(ch)
     if ch not in answer:
-        print("There are no " + ch + "'s")
+        print("There are no {}'s".format(ch))
     else:
         pos_s = list(filter(lambda x: x >= 0, [i if c == ch else -1 for i, c in enumerate(answer)]))
         display = ''.join([ch if i in pos_s else c for i, c in enumerate(display)])
         if '-' not in display:
+            print('CORRECT!!')
             break
     print(display)
     n_tries -= 1
