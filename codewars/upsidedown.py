a82833bc18798218f622458efec118ce71eb37b4
Naoyuki Kumagai <kumagai@mimas>
2018-03-05 09:01:11 +0900

    [codewars/upsidedown] Refactor by removing unnecessary codes

--- codewars/upsidedown.py
+++ codewars/upsidedown.py
@@ -2,19 +2,6 @@ import re
 from functools import reduce
 import operator
 
-def count_all_for(num_digits):
-    n_for_mid = 1
-    if num_digits % 2 == 1:
-        n_for_mid = 3  # 0, 1, 8
-        num_digits -= 1
-    num_digits //= 2
-    n_for_top = 4 if num_digits > 0 else 1  # 1, 8, 6, 9
-    n_for_rest = max(5 ** (num_digits - 1), 1)  # 0, 1, 8, 6, 9
-    return n_for_top * n_for_rest * n_for_mid
-    n = 10 ** (num_digits - 1)
-    #n = 10 ** num_digits - 1
-    return count_for(n, is_greater=True)
-
 def swap_digits_all(s, d1, d2):
     d1, d2 = map(str, (d1, d2))
     c_tmp = 'x'
@@ -28,6 +15,7 @@ def is_upsidedown(n):
     return s == swap_digits_all(s_rev, 6, 9)
 
 debug = 0
+
 def count_for_same_num_digits_as(s_num, is_greater, allows_zero_at_top=False):
 
     if debug: print('"{} {}" ==> '.format('>' if is_greater else '<', s_num), end='')
@@ -46,34 +34,14 @@ def count_for_same_num_digits_as(s_num, is_greater, allows_zero_at_top=False):
             nums += ('00',)
         retval = f_count(s_num, nums)
     else:
-        """
-        1.0  ==>  a * 3 + s * 1    1 6 8 9
-        1.1  ==>  a * 3 + s * 1    1 6 8 9   1
-        1.2  ==>  a * 3 + s * 0      6 8 9   1
-        6.8  ==>  a * 3 + s * 1      6 8 9   1
-        6.9  ==>  a * 3 + s * 1      6 8 9   1 6
-        7.0  ==>  a * 3 + s * 1        8 9   1 6
-        8.7  ==>  a * 3 + s * 1        8 9   1 6
-        8.8  ==>  a * 3 + s * 1        8 9   1 6 8
-        8.9  ==>  a * 3 + s * 1          9   1 6 8
-        9.5  ==>  a * 3 + s * 1          9   1 6 8
-        9.6  ==>  a * 3 + s * 1          9   1 6 8 9
-        9.7  ==>  a * 3 + s * 1              1 6 8 9
-        """
         d1, d2 = s_num[0], s_num[-1]
-        """
-        if d1 == 6:
-            d = d2 - (9 - 6)
-        elif d1 == 9:
-            d = d2 + (9 - 6)
-        else:
-            d = f_choose(d1, d2)
-        """
         digits = ('1', '6', '8', '9')
         if allows_zero_at_top:
             digits += ('0',)
         count_d1 = f_count(d1, digits)
+
         if debug: print("(count_d1 = {})".format(count_d1))
+
         count = 0
         str_num_max_or_min = ('0' if is_greater else '9') * (length - 2)
         count_all_for_two_digit_less = count_for_same_num_digits_as(str_num_max_or_min, is_greater, allows_zero_at_top=True)
@@ -90,19 +58,12 @@ def count_for_same_num_digits_as(s_num, is_greater, allows_zero_at_top=False):
             elif d1 == '9':
                 d2 = int(d2) + (9 - 6)
             if f_cmp_ne(int(d2), int(d1)) and is_upsidedown(s_num_two_less):
-                #s_num_two_less == max_or_min_sub_str_num(len(s_num_two_less), is_max=is_greater) or s_num_two_less == '080':
                 count -= 1
         retval = count
-    if debug: print(retval)
-    return retval
 
-def max_or_min_sub_str_num(num_digits, is_max=True):
-    if is_max:
-        n_half = num_digits // 2
-        mid = '8' if num_digits % 2 == 1 else ''
-        return '9' * n_half + mid + '6' * n_half
-    else:
-        return '0' * num_digits
+    if debug: print(retval)
+
+    return retval
 
 def first_half_and_mid_and_second_half_of(n):
     s = str(n)
@@ -137,37 +98,6 @@ if __name__ == '__main__':
     import sys
     sys.path.insert(0, './codewars')
     import test
-
-    """
-    debug = 1
-
-    test.assert_equals(upsidedown('909080743', '1000000000'),312)
-    quit()
-
-    ('0','10'),
-    ('6','25'),
-    ('10','100'),
-    ('100','1000'),
-
-    args = (
-        ('1234','12345'),
-    )
-    for arg in args:
-        a1 = upsidedown(*arg)
-        a2 = upsidedown_brutally(*arg)
-        print("{}-{} : {} ({})".format(arg[0], arg[1], a1, a2))
-    quit()
-
-    for n in range(1, 11):
-        actual = upsidedown('1' + '0' * (n - 1), '9' * n)
-        print("{0:2}: {1} ({2})".format(n, count_all_for(n), actual))
-    quit()
-
-    count_for_same_num_digits_as('12345678900000000', is_greater=False)
-    #                             11999999866666611
-    #                             16000000000000091
-    quit()
-    """
 
     test.assert_equals(upsidedown('0','10'),3)
     test.assert_equals(upsidedown('6','25'),2)
